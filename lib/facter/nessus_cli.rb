99c72e8e4e76e0f75c17734e575cfe614a5681df
Adam Stephens <adam.c.stephens@noaa.gov>
2014-12-17 13:41:11 -0500

    nessuscli is in /opt/nessus/sbin

--- lib/facter/nessus_cli.rb
+++ lib/facter/nessus_cli.rb
@@ -4,6 +4,8 @@ Facter.add('nessus_cli') do
     result = false
     if File.exists? '/opt/nessus/bin/nessuscli'
       result = true
+    elsif File.exists? '/opt/nessus/sbin/nessuscli'
+      result = true
     end
     result
   end
