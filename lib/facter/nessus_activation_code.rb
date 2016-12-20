96310f9d5e51ff62049314da353d0bce1afc0bc1
Vincent Deygas <Vincent.Deygas@rewardgateway.com>
2016-12-20 11:54:28 +0000

    Optimise nessus_activation_code fact
    * Remove '\n' at the end of the value (so it can be used in tests)
    * Use 'fix --list' and grep the output instead of using
      'fetch --code-in-use' as the format is easier to parse
    * Fix the split pattern so it works with the new nessus client

--- lib/facter/nessus_activation_code.rb
+++ lib/facter/nessus_activation_code.rb
@@ -2,14 +2,14 @@ Facter.add('nessus_activation_code') do
   confine :kernel => 'Linux'
   setcode do
     if File.exist? '/opt/nessus/bin/nessus-fetch'
-      output = `/opt/nessus/bin/nessus-fetch --code-in-use 2>/dev/null` 
-      output.split(' : ')[-1]
+      output = `/opt/nessus/bin/nessus-fetch --code-in-use 2>/dev/null`
+      output.chomp.split(': ')[-1]
     elsif File.exists? '/opt/nessus/bin/nessuscli'
-      output =  `/opt/nessus/bin/nessuscli fetch --code-in-use 2>/dev/null`
-      output.split(' : ')[-1]
+      output =  `/opt/nessus/bin/nessuscli fix --list --secure 2>/dev/null | grep '^registration_code:'`
+      output.chomp.split(': ')[-1]
     elsif File.exists? '/opt/nessus/sbin/nessuscli'
-      output =  `/opt/nessus/sbin/nessuscli fetch --code-in-use 2>/dev/null`
-      output.split(' : ')[-1]
+      output =  `/opt/nessus/sbin/nessuscli fix --list --secure 2>/dev/null | grep '^registration_code:'`
+      output.chomp.split(': ')[-1]
     end
   end
 end
