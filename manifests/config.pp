94483af9870a7bc40108d41ee71878f53d098789
Allard Berends <aberends@casema.nl>
2020-02-12 15:38:11 +0100

    Removed WARN Unknown variable nessus_activation_code

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,24 +8,43 @@ class nessus::config {
   }
 
   if $nessus::activation_code {
-    # This nessus is not yet activated, let's do it!
-    if $::nessus_activation_code != $nessus::activation_code {
-      # This nessus is not yet activated or is using a different activation code, let's add/update it!
-      $activate_command = "nessuscli fetch --register-only ${nessus::activation_code}"
-
-      exec { 'Activate Nessus':
-        path    => [ '/opt/nessus/bin', '/opt/nessus/sbin' ],
-        command => $activate_command,
-        notify  => Exec['Wait 60 seconds for Nessus activation'],
+    # Nessus activation code is present in configuration.
+    if ($facts['nessus_activation_code'] == undef) {
+      # Custom fact "nessus_activation_code" has not been
+      # set, i.e. Nessus needs to be activated.
+      $run_activate_nessus = true
+    } else {
+      # Custom fact "nessus_activation_code" exists.
+      if ($::nessus_activation_code != $nessus::activation_code) {
+        # New Nessus activation code presented. Activate
+        # Nessus again.
+        $run_activate_nessus = true
+      } else {
+        # Current activation code is still current.
+        $run_activate_nessus = false
       }
     }
+  } else {
+    # Without Nessus activation code we cannot activate
+    # Nessus.
+    $run_activate_nessus = false
   }
 
-  # Wait, then restart nessusd after activation
-  exec { 'Wait 60 seconds for Nessus activation':
-    path        => [ '/bin' ],
-    command     => 'sleep 60',
-    refreshonly => true,
-    notify      => Service[$nessus::service_name],
+  if $run_activate_nessus {
+    $activate_command = "nessuscli fetch --register-only ${nessus::activation_code}"
+
+    exec { 'Activate Nessus':
+      path    => [ '/opt/nessus/bin', '/opt/nessus/sbin' ],
+      command => $activate_command,
+      notify  => Exec['Wait 60 seconds for Nessus activation'],
+    }
+
+    # Wait, then restart nessusd after activation
+    exec { 'Wait 60 seconds for Nessus activation':
+      path        => [ '/bin' ],
+      command     => 'sleep 60',
+      refreshonly => true,
+      notify      => Service[$nessus::service_name],
+    }
   }
 }
